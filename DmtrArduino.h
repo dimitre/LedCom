98e8b5052fd1e130018e58c991f322688b82a0a0
Dimitre <dimitre79@gmail.com>
2016-12-25 06:24:40 -0200

    up

--- DmtrArduino.h
+++ DmtrArduino.h
@@ -16,6 +16,16 @@ fader
 //#include "Timer.h"
 
 
+#define BUTTON_PIN 4
+#define NBUTTONS 6
+#define DMX_PIN 12
+
+#define NCOLORS 5
+
+
+bool debug = true;
+
+
 float dmx[12];
 float dmxEasy[12];
 
@@ -35,12 +45,9 @@ float tempo = 0;
 float lastTempo = 0;
 
 
-#define BUTTON_PIN 4
-#define NBUTTONS 6
-#define DMX_PIN 12
 
 
-int colors[5][3] = {
+int colors[NCOLORS][3] = {
   { 255, 255, 255 }, //branco
   { 255, 0, 0 }, //vermelho
   { 255, 0, 40 }, //magentinha
@@ -69,31 +76,50 @@ void pisca() {
     dmx[a * 3 + 2] = ligado ? blue : 0;
   }
   conta ++;
+  digitalWrite(LED_BUILTIN, conta % 2 ? LOW : HIGH);
 }
 
+float somaTempo = 0;
+int tapCount = 0;
 
 void bpmTap() {
-  Serial.println("Button");
   tempo = millis() - lastTempo;
   lastTempo = millis();
   Serial.println(tempo);
-  if (tempo > 0) {
-    t.stop(evento);
+  if (tempo > 0 && tempo < 1000) {
+    Serial.println("tap" + String(tapCount));
+    somaTempo += tempo;
+    tapCount ++;
+  } else {
+  	tapCount = 0;
+  	somaTempo = 0;
+  }
+
+  int nTap = 4;
+  if (tapCount == nTap) {
+  	Serial.println("resetTempo");
+  	tempo = somaTempo / (float) nTap;
+  	t.stop(evento);
     evento = t.every(tempo, pisca);
-    Serial.println("pisca");
+    tapCount = 0;
+    somaTempo = 0;
   }
   Serial.println("BpmTap" + String(tempo));
 }
 
 void buttonDown(int a) {
-  if (a == 4) {
+  if (a == 1) {
     bpmTap();
   }
-  Serial.println("button down ::: " + String(a));
+  if (debug) {
+  	Serial.println("button down ::: " + String(a));
+  }
 }
 
 void buttonUp(int a) {
-  Serial.println("button up ::: " + String(a));
+	if (debug) {
+		Serial.println("button up ::: " + String(a));
+	}
 }
 
 void readButtons() {
@@ -127,12 +153,16 @@ void readPots() {
       }
 
       else if (indexPot == 16) { //segundo pot
-        easing = map(analogRead(16), 5, 1023, 0, 60);
-        Serial.println(easing);
+        easing = map(analogRead(16), 5, 1023, 0, 30);
+        if (debug) {
+        	Serial.println(easing);
+        }
       }
       else if (indexPot == 15) { //terceiro pot
-        cor =  map(analogRead(15), 5, 1023, 0, 3);
-        Serial.println(cor);
+        cor =  map(analogRead(15), 5, 1023, 0, NCOLORS);
+        if (debug) {
+	    	Serial.println(cor);
+	    }
       }
 
       else if (indexPot == 14) { //quarto pot
