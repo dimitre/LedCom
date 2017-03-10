5b55d9d85f8839fed07ece2eeb0621a34c37f4b6
Dimitre <dimitre79@gmail.com>
2017-03-10 08:58:00 -0300

    updates

--- Classes.h
+++ Classes.h
@@ -1,48 +1,149 @@
-enum model {
-	LED, 
-	LINHA,
-	MOVINGHEAD, 
-};
-
-struct color {
-public:
-	unsigned int r;
-	unsigned int g;
-	unsigned int b;
-	color (int rr, int gg, int bb) : r(rr), g(gg), b(bb) {};
-
-	// color (int rr = 255, int gg = 255, int bb = 255) {
-	// 	r = rr;
-	// 	g = gg;
-	// 	b = bb;
-	// }
-};
 
 class fixture {
 	public:
-		// unsigned int channel;
-		// unsigned int canalRgb;
-		// unsigned int rand;
+		uint16_t channel;
+		uint16_t port;
+		bool buttonState = true;
+		bool sobe = false;
+		byte index;
 
-		// sera problema?
+		Bounce debounce;
 
-		byte channel;
-		model modelo;
-		int x, y;
-		byte canalRgb;
-		byte rand;
+		bool ready = false;
 
-		float xx, yy; 
-		byte indice;
-		float qual;
-		// fixture (int ch = 1, model mod = LED, int estex=0, int estey=0) {
-		// 	channel = ch;
-		// 	modelo = mod;
-		// 	x = estex;
-		// 	y = estey;
+		void (*mover)() = NULL;
+		void (*parar)() = NULL;
+		void (*panic)() = NULL;
 
-		fixture (byte ch = 1, model mod = LED, int estex=0, int estey=0) : channel(ch), modelo(mod), x(estex), y(estey) {
-			rand = random(0,3);
-			canalRgb = modelo == MOVINGHEAD ? ch + 3 : ch;
+
+		fixture (uint16_t ch = 1, uint16_t p = 1, byte i = 1) : channel(ch), port(p), index(i) {
+			pinMode(port, INPUT_PULLUP);
+			//debounce = Bounce();
+			//debounce = Bounce (button, 8);
+			debounce.attach(port, 8);
+		}
+
+		void setDmx(int c, int v) {
+			DmxSimple.write(c + channel, byte(v));
+		}
+
+		void para() {
+			// para movimento
+			setDmx(5, 0);
+		}
+
+		void move() {
+			if (channel > 0) {
+				sobe = digitalRead(SOBEDESCE_PIN) == LOW;
+								Serial.println(sobe ? "SUBINDO" : "DESCENDO");
+
+				if (sobe) {
+					setDmx(4, 200);
+				} else {
+					setDmx(4, 0);
+				}
+				// aciona movimento
+				setDmx(5, 200);
+			} else {
+				if (ready) {
+					if (port == 33) {
+						(*mover)();
+						Serial.println("MOVER");
+					}
+					else if (port == 22) {
+						Serial.println("PANIC");
+						(*panic)();
+					}
+				}
+				//xaxa
+				//if ((*mover))
+			}
+		}
+
+		// void aciona() {
+		// 	if (digitalRead(port) == LOW) {
+		// 		move();
+		// 	}
+
+		// 	else {
+		// 		para();
+		// 	}
+		// }
+
+		void readButton() {
+			debounce.update();
+			if (debounce.fallingEdge()) {
+				move();
+				Serial.print(sobe ? "SOBE :: " : "DESCE :: ");
+				Serial.println(String(channel) + "\tbutton :: " + String(port) + "\tindex :: " + String(index));
+			}
+			if (debounce.risingEdge()) {
+				para();
+				Serial.println("Para :: " + String(channel));
+
+			}
+
+			// if (buttonState != digitalRead(button)) {
+			// 	aciona();
+			// 	buttonState = digitalRead(button);
+			// }
 		}
 };
+
+
+
+
+
+class button {
+public:
+	uint16_t port;
+	//int tipo;
+	//bool buttonState;
+	//bool sobe = false;
+	Bounce debounce;
+
+	void (*mover)() = NULL;
+	void (*parar)() = NULL;
+
+	button( uint16_t b) : port(b) {
+		pinMode(port, INPUT_PULLUP);
+		debounce.attach(port, 8);
+		Serial.println("debounce button port " + String(port));
+	}
+
+	void readButton() {
+		//Serial.println("readbutton botao");
+		//sobe = digitalRead(SOBEDESCE_PIN) == LOW;
+		//Serial.println(digitalRead(port));
+		debounce.update();
+		if (debounce.fallingEdge()) {
+			Serial.println("aperta");
+			// if ((*mover) != NULL) {
+			// 	(*mover)();
+			// }
+		}
+		if (debounce.risingEdge()) {
+			Serial.println("solta");
+			// if ((*parar) != NULL) {
+			// 	(*parar)();
+			// }
+		}
+
+		// if (buttonState != digitalRead(port)) {
+		// 	if (digitalRead(port) == LOW) {
+		// 		if (sobe) {
+		// 			if ((*subir) != NULL) {
+		// 				(*subir)();
+		// 			}
+		// 		} else {
+		// 			(*descer)();
+		// 		}
+		// 	} else {
+		// 		(*parar)();
+		// 	}
+		// 	//Serial.println(tipo);
+		// 	buttonState = digitalRead(port);
+		// }
+	}
+};
+
--- Definitions.h
+++ Definitions.h
@@ -1,72 +1,49 @@
-color black = color(0,0,0);
+// todos 33
 
-color colors[] = { 
-	color(255, 255, 255),  // 	0 — branco
-	color(255, 0, 0), // 		1 — vermelho
-	color(255, 0, 40), //		2 — magentinha
-	color(0,255,40), //			3 — verdito
-	color(0, 255, 255), // 		4 — azul claro
-	color(0, 255, 0), // 		5 — g
-	color(0, 0, 255), // 		6 — b	
-	color(130, 63, 212), // 		7 — roxo lindo
-	color(202, 255, 16), // 		8 — verde limao
-	color(130, 255, 212), // 		9 — verde clarissimo
-	color(255, 20, 212), // 		10 — rosinha
+button buttons[] = {
+	// button (33),
+	// button (22)
 };
 
-int paletas[][3] = {
-	{ 0, 0, 0 },  // branco
-	{ 1, 1, 1 },  // vermelho
-	{ 2, 2, 2 },  // magenta
-	{ 3, 3, 3 },  // verde
-	{ 4, 4, 4 },  // azulclaro
-	{ 5, 5, 5 },  // g
-	{ 6, 6, 6 },  // b
-	{ 7, 7, 7 },  // roxo lindo
-	{ 8, 8, 8 },  // verde limao
-	{ 9, 9, 9 },  // verde clarissimo
-	{ 10, 10, 10 },  // rosinha
-	{ 0, 1, 4 },  // branco verm azul claro
-	{ 2, 3, 3 },  // magenta verde verde
-	{ 0, 1, 1 },  // branco verm verm
-	{ 0, 4, 2 },  // branco azul magenta
-	{ 7, 7, 3 }	  // roxo roxo verde
 
-		// 12 paletas?
-};
 
 fixture fixtures[] = {
-	fixture(01 , LINHA, 0, 1),
-	fixture(04 , LINHA, 0, 4),
-	fixture(07 , LINHA, 0, 7),
-	fixture(10 , LINHA, 0, 10),
 
-	fixture(13 , LINHA, 5, 1),
-	fixture(16 , LINHA, 5, 4),
-	fixture(19 , LINHA, 5, 7),
-	fixture(22 , LINHA, 5, 10),
+	fixture(0, 33, 0),
+	fixture(0, 22, 0),
+
+	fixture(1, 35, 1),
+	fixture(11, 36, 2),
+	fixture(21, 37, 3),
+	fixture(31, 38, 4),
+	
+	fixture(41, 39, 5),
+	fixture(51, 34, 6),
+	fixture(61, 21, 7),
+	fixture(71, 14, 8),
+
+	fixture(81, 15, 9),
+	fixture(91, 16, 10),
+	fixture(101, 17, 11),
+	fixture(111, 18, 12),
+
+	fixture(121, 19, 13),
+	fixture(131, 20, 14),
 
 
-	fixture(25 , LED, 3, 3), //ok, bola na frente dos p.a.s
-	fixture(28 , LED, 3, 4), // segunda bola em frente ao p.a, menorzinha
-	fixture(31 , LED, 2, 5), // terceira bola em frente ao pa, grande
-	fixture(34 , LED, 4, 0), //bola zero, grande
 
-	fixture(37 , LED, 3, 1), // bola acima do dj, grande
-	fixture(40 , LED, 2, 0), // bola acima da cozinha, pequena
-	fixture(43 , LED, 1, 3), // canal faltante
-	fixture(46 , LED, 2, 9), // bola acima do bar, grande, na frente da placa de madeira
 
-	fixture(49 , LED, 1, 10), // bola logo a esquerda da ultima
-	fixture(52 , LED, 0, 11), // pequena na esquerda
-	fixture(55 , LED, 2, 11), // fundo
-	fixture(58 , LED, 4, 9), // pequena direita
+	// botao da esquerda 33, botao da direita 22
+	// sobedesce 31
+	// fixture(141, 0, 8),
 
-	fixture(61, LED, 4, 9), // bolinha pouco a esquerda de miller
-	fixture(64, LED, 3, 11), // grande ao lado da arvore
-	fixture(67, LED, 4, 12), // ultima bola da direita
-	fixture(70, LED, 2, 11), 
+	// fixture(161, 1),
+	// fixture(181),
+	// fixture(201),
+	// fixture(221),
 
-	fixture( 73, MOVINGHEAD),
-	fixture( 81, MOVINGHEAD)
+	// fixture(241),
+	// fixture(261),
 };
+
+//do a 15 ao a 21
